7b12d5c4f2453a42a2e2c42b0eb4403883c50b9c
Kumar Anand <kranand9430@gmail.com>
2025-07-30 19:52:10 +0530

    Initialise

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom"
+// import Navbar from "./Components/Navbar"
+// import Footer from "./Components/Footer"
+import { createContext, useState } from 'react'
+
+
+export const UserContext = createContext();
+
+function App() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+    <UserContext.Provider value={{ open, setOpen }}>
+      {/* <Navbar /> */}
+      <Outlet />
+      {/* <Footer /> */}
+      </UserContext.Provider>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 2xl:px-48 gap-12 '>
+        <h1 className='text-4xl font-bold'>This is Basic react setup</h1>
+        <p>Here I have set-up Vite using JS, Tailwind, React-icons, React-router-dom</p>
+    </div>
+  )
+}
+
+export default Home
